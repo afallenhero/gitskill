274710f6839adeb39690d6e8d37cbb314cb01a40
afallenhero <36064999+afallenhero@users.noreply.github.com>
2019-08-09 16:15:30 +0800

    Create readme.mk
    在github上新建一个仓库

---
+++ readme.mk
@@ -0,0 +1,6 @@
+echo "# gitskill" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/afallenhero/gitskill.git
+git push -u origin master
